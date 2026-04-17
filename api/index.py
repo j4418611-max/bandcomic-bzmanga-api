7034dc8981720e336d8c9c807f52cec8730b4430
j4418611-max <j4418611@gmail.com>
2026-04-17 22:54:31 +0800

    更新 index.py

--- api/index.py
+++ api/index.py
@@ -131,6 +131,4 @@ def search_comics(keyword, page):
 def index():
     return jsonify({"status": "腕上漫画-包子漫画API运行中", "docs": "访问 /config 查看配置"})
 
-#Vercel部署不需要这行
-#if __name__ == '__main__':
-#    app.run(debug=True)
+
